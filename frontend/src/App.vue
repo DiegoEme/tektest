89608691cb12984f66802fe97e20f0aa45c91195
Diego <soterg.uhdeia@gmail.com>
2021-01-16 23:12:17 -0500

    Vuetify project DataTable added

---
+++ frontend/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+       <hello-world></hello-world>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script>
+
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  
+};
+</script>
